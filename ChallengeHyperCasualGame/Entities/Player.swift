32ded7d4f36db5675f7c0e6b0dee56b9f4fa578e
jasonmiracleg <jasonmiracle15@gmail.com>
2025-07-22 11:28:37 +0700

    Merge Player Control

---
+++ ChallengeHyperCasualGame/Entities/Player.swift
@@ -0,0 +1,157 @@
+//
+//  PlayerFactory.swift
+//  ChallengeHyperCasualGame
+//
+//  Created by Akmal Ariq on 15/07/25.
+//
+
+import SpriteKit
+
+class Player : SKNode {
+    
+    let bottleBody: SKShapeNode
+    let bottleCap: SKShapeNode
+    let topSensor: SKNode
+    let bottomSensor: SKNode
+    
+    init(in scene: SKScene) {
+        // Dimensions
+        let bodySize = CGSize(width: 20, height: 40)
+        let capSize = CGSize(width: 10, height: 5)
+        
+        // Make Shapes
+        bottleBody = SKShapeNode(rectOf: bodySize, cornerRadius: 6)
+        bottleCap = SKShapeNode(rectOf: capSize)
+        
+        bottomSensor = SKNode()
+        topSensor = SKNode()
+
+        super.init()
+        
+        self.position = CGPoint(x: scene.frame.midX, y: 100)
+        
+        setupBottleBody()
+        setupBottleCap(offsetY: bodySize.height/2 + capSize.height/2)
+        setupPhysics(size: bodySize)
+        setupPhysics(size: bodySize)
+        
+        self.addChild(bottleBody)
+        self.addChild(bottleCap)
+        self.addChild(bottomSensor)
+        self.addChild(topSensor)
+
+        scene.addChild(self)
+    }
+    
+    private func setupBottleBody() {
+        bottleBody.fillColor = .gray
+        bottleBody.strokeColor = .clear
+        bottleBody.position = .zero
+    }
+    
+    private func setupBottleCap(offsetY: CGFloat) {
+        bottleCap.fillColor = .darkGray
+        bottleCap.strokeColor = .clear
+        bottleCap.position = CGPoint(x: 0, y: offsetY)
+    }
+    
+    private func setupPhysics(size: CGSize) {
+        let physicsBody = SKPhysicsBody(rectangleOf: size)
+        physicsBody.linearDamping = 1.0
+        physicsBody.friction = 1.0
+        physicsBody.restitution = 0.0
+        physicsBody.allowsRotation = true
+        physicsBody.categoryBitMask = 0x1 << 0
+        physicsBody.contactTestBitMask = 0x1 << 1
+        physicsBody.collisionBitMask = 0x1 << 1
+
+        self.physicsBody = physicsBody
+    }
+    
+    private func setupSensors(size: CGSize) {
+        // Bottom sensor
+        bottomSensor.position = CGPoint(x: 0, y: -size.height / 2)
+        let bottomPhysics = SKPhysicsBody(rectangleOf: CGSize(width: size.width * 0.8, height: 2))
+        bottomPhysics.isDynamic = false
+        bottomPhysics.affectedByGravity = false
+        bottomPhysics.categoryBitMask = 0x1 << 2
+        bottomPhysics.contactTestBitMask = 0x1 << 1
+        bottomPhysics.collisionBitMask = 0
+        bottomPhysics.usesPreciseCollisionDetection = true
+        bottomSensor.physicsBody = bottomPhysics
+
+        // Top sensor
+        topSensor.position = CGPoint(x: 0, y: size.height / 2)
+        let topPhysics = SKPhysicsBody(rectangleOf: CGSize(width: size.width * 0.8, height: 2))
+        topPhysics.isDynamic = false
+        topPhysics.affectedByGravity = false
+        topPhysics.categoryBitMask = 0x1 << 3
+        topPhysics.contactTestBitMask = 0x1 << 1
+        topPhysics.collisionBitMask = 0
+        topPhysics.usesPreciseCollisionDetection = true
+        topSensor.physicsBody = topPhysics
+    }
+
+    func handleJump(from startPos: CGPoint, to endPos: CGPoint) {
+        guard let body = self.physicsBody else { return }
+        let velocity = body.velocity
+        let speedThreshold: CGFloat = 1
+        let isIdle = abs(velocity.dy) < speedThreshold
+
+        if isIdle {
+            let dx = endPos.x - startPos.x
+            let dy = endPos.y - startPos.y
+            
+            let velocityX = -dx * 4
+            let velocityY = min(-dy * 7, 1300)
+
+            body.velocity = CGVector(dx: velocityX, dy: velocityY)
+            
+            var spin = 0.0
+            if dx > 0 {
+                spin = dx * 0.1
+            } else {
+                spin = dx * 0.1
+            }
+            body.angularVelocity = spin
+        }
+    }
+    
+    func handleSpin(from startPos: CGPoint, to endPos: CGPoint) {
+        guard let body = self.physicsBody else { return }
+
+        let velocity = body.velocity
+        let speedThreshold: CGFloat = 1
+        let isIdle = abs(velocity.dy) < speedThreshold
+
+        if isIdle {
+            let dx = startPos.x - endPos.x
+            let dy = startPos.y - endPos.y
+            
+            let velocityX = dx * 4
+            let velocityY = min(dy * 7, 1300)
+
+            body.velocity = CGVector(dx: velocityX, dy: velocityY)
+            
+            var spin = 0.0
+            if dx > 0 {
+                spin = dx * 0.1
+            } else {
+                spin = dx * 0.1
+            }
+            body.angularVelocity = spin
+        }
+    }
+    
+    func wrapAroundEdges(in scene: SKScene) {
+        if position.x < -50 {
+            position.x = scene.frame.width + 50
+        } else if position.x > scene.frame.width + 50 {
+            position.x = -50
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
